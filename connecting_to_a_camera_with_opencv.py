17659634a0d5d61435bced9b847bdbb2c5b7b1a5
Desire Iradukunda <35916017+Desire100@users.noreply.github.com>
2019-02-17 00:21:02 +0800

    Create connecting_to_a_camera_with_opencv.py

---
+++ connecting_to_a_camera_with_opencv.py
@@ -0,0 +1,37 @@
+# Image basics with opencv
+
+import cv2
+
+capture = cv2.VideoCapture(0)
+
+import cv2
+
+
+# Connects to your computer's default camera
+cap = cv2.VideoCapture(0)
+
+
+# Automatically grab width and height from video feed
+# (returns float which we need to convert to integer for later on!)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+while True:
+    
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frameq
+    cv2.imshow('frame',gray)
+    
+    # This command let's us quit with the "q" button on a keyboard.
+    # Simply pressing X on the window won't work!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture and destroy the windows
+cap.release()
+cv2.destroyAllWindows()
